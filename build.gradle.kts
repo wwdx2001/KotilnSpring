3cf71225001c550cdec404a915119c75af9dca46
赵启航 <zqh99999a@163.com>
2026-05-22 00:09:26 +0800

    修改gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
 	}
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
